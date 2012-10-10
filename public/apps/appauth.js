fb57afbae7cd1935aebc5c37071f3e3ca176fecd
Jonathan Dunlap <jdunlap@outlook.com>
2012-10-10 16:20:52 -0700

    auth app int fixes

--- public/apps/appauth.js
+++ public/apps/appauth.js
@@ -16,10 +16,17 @@ function handleSysMsg(msg) {
 
 function handleAppMsg(msg) {
 	var data = msg.split(" ");
+	data[0] = data[0].toLowerCase();
 	if (data[0] == "name" || data[0] == "n") {
 		$("input[name=user]").val(data[1]);
 	}
-	if (data[0] == "p" || data[0] == "pw" || data[0] == "pass" || data[0] == "password") {
+	if (data[0] == "p" || data[0] == "pw" || data[0].indexOf("pass")!=-1) {
 		$("input[name=passwd]").val(data[1]);
+	}
+	if (data[0].indexOf("reg")!=-1 || data[0] == "r" || data[0].indexOf("new")!=-1) {
+		$("input[name=login]").click();
+	}
+	if (data[0].indexOf("back")!=-1 || data[0] == "l" || data[0].indexOf("log")!=-1) {
+		$("input[name=login]").click();
 	}
 }

b7370745916804451ff8f23fb94921e41515fa66
Jonathan Dunlap <jdunlap@outlook.com>
2012-10-22 15:19:36 -0700

    app has state

--- public/apps/appvote.js
+++ public/apps/appvote.js
@@ -13,11 +13,17 @@ function onRdy() {
 }
 
 var voteData = {};
+var usersVoted = {};
 function handleMsg(msg) {
 	if(msg.type!="app") return;
 	var cmd = msg.cmd;
 	var data = msg.msg;
 	if (cmd == "vote") {
+		if(usersVoted[msg.user]) {
+			alert("already voted"); 
+			return;
+		}
+		usersVoted[msg.user] = data;
 		//alert("a "+data[1]);
 		var c = voteData[data] || 0;
 		voteData[data] = c + 1;
@@ -25,8 +31,14 @@ function handleMsg(msg) {
 		var comp = $('#tally');
 		comp.html('');
 		for (var i in voteData) {
-			comp.html(comp.html() + " " + i + ":" + voteData[i])
+			comp.append(" " + i + " has " + voteData[i] +"# votes")
 		}
+		comp.append("<h5>Users:</h5>");
+		for (var i in usersVoted) {
+			comp.append(" " + i + " voted " + usersVoted[i])
+		}
+		//comp.append("</p>");
+		cf.resize();
 		//$("input[name=VoteGroup]").attr("disabled", "disabled"); // only for auth user
 	}
 }
